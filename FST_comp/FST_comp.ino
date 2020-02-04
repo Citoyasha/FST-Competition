6e119cb2809d99049dd47bce9e011dc59a91ed95
Citoyasha <citoyasha@mail.ru>
2020-02-04 23:00:21 +0100

    second update, added ultrasound & test of 2nd circuit

--- FST_comp/FST_comp.ino
+++ FST_comp/FST_comp.ino
@@ -9,6 +9,9 @@ int LT = 8;
 int hrt=6;
 int hlt=9;
 int val1,val2,val11,val22;
+int trigPin = 12;    // Trigger
+int echoPin = 13;    // Echo
+long duration, cm;
 void setup() {
   Serial.begin(9600);
   pinMode(IN1, OUTPUT);
@@ -21,28 +24,51 @@ void setup() {
   pinMode(LT, INPUT);
   pinMode(hrt, INPUT);
   pinMode(hlt, INPUT);
-  analogWrite(ENA,70);
-  analogWrite(ENB,70);
+  analogWrite(ENA,85);
+  analogWrite(ENB,85);
   Serial.write("Starting!");
+  Serial.begin (9600);
+  //Define inputs and outputs
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void loop() {
-   val1 = digitalRead(RT); //put the value of right tracker in val1
-   val2 = digitalRead(LT); //put the value of left tracker in val2
-   val11 = digitalRead(hrt); //put the value of right tracker in val1
-   val22 = digitalRead(hlt); //put the value of left tracker in val2
-
-
+   val1 = digitalRead(RT); 
+   val2 = digitalRead(LT); 
+   val11 = digitalRead(hrt); 
+   val22 = digitalRead(hlt); 
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+   duration = pulseIn(echoPin, HIGH);
+    cm = (duration/2) / 29.1;     
+    Serial.println(cm);
+  if(cm > 26){ 
     if((val1==val2))
       Fwd(); 
     if((val1==0)&&(val2==1))
       Left();
     if((val1==1)&&(val2==0))
       Right();
-    if((val11==0)&&(val22==1))
+    if((val11==0)&&(val22==1)&&(val1==0)&&(val2==1))
       Hard_Left();
-    if((val11==1)&&(val22==0))
-      Hard_Right();   
+    if((val11==1)&&(val22==0)&&(val1==1)&&(val2==0))
+      Hard_Right();  
+  }else if(cm<26){
+    if((val1==val2))
+      Fwd(); 
+    if((val1==1)&&(val2==0))
+      Left();
+    if((val1==0)&&(val2==1))
+      Right();
+    if((val11==1)&&(val22==0)&&(val1==1)&&(val2==0))
+      Hard_Left();
+    if((val11==0)&&(val22==1)&&(val1==1)&&(val2==1))
+      Hard_Right();  
+  }
 
 }
 void Fwd(){
@@ -77,17 +103,17 @@ void Stop(){
 }
 void Hard_Right(){
   Fwd();
-delay(80);
+delay(400);
 Right();
-delay(800);
+delay(600);
 Stop (); 
-delay (50);
+delay (70);
 }
 void Hard_Left(){
   Fwd();
-delay(80);
+delay(400);
 Left();
-delay(800);
+delay(600);
 Stop (); 
-delay (50);
+delay (70);
 }
